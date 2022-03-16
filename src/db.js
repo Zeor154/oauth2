018a75456a3984241b8d2881e4fff2fc1c8f73b0
Zeor <89521852+Zeor154@users.noreply.github.com>
2022-03-16 19:48:01 +0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const { connect } = require('mongoose');
 
-connect('mongodb+srv://myuser:<password>@cluster0.5s0n3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+connect('mongodb+srv://myuser:userman12@cluster0.5s0n3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 ')
     .then(() => { console.log('Connect to mongodb') })
     .catch((err) => { console.log(err) })
